a0be726c19001da9f62f5cd342d1762dee91b944
Tyago Goulart <tyagogoulartn@gmail.com>
2017-06-02 00:00:55 -0300

    avanço de conteúdo, metódos etc

--- src/components/instagram-card/instagram-card.component.ts
+++ src/components/instagram-card/instagram-card.component.ts
@@ -1,4 +1,9 @@
+import { MediaPage } from './../../pages/media/media';
+import { InstagramService } from './../../providers/instagram.service';
+import { UserProfilePage } from './../../pages/user-profile/user-profile';
+import { NavController, AlertController } from 'ionic-angular';
 import { Component, Input } from '@angular/core';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'instagram-card',
@@ -9,7 +14,41 @@ export class InstagramCardComponent {
   @Input() public medias;
   @Input() public isGrade;
   @Input() public imgWidth;
+  @Input() public credentials;
 
-  constructor() {}
+  constructor(
+    public navCtrl: NavController,
+    public instaService: InstagramService,
+    public alertCtrl: AlertController
+  ) {}
+
+  showUserProfile(userId) {
+    this.navCtrl.push(UserProfilePage, {
+      userId: userId,
+      credentials: this.credentials
+    });
+  }
+
+  showMedia(media) {
+    this.navCtrl.push(MediaPage, {
+      media: media
+    });
+  }
+
+  likeMedia(mediaId) {
+    this.instaService.addLikeToMedia(mediaId, this.credentials.access_token)
+      .subscribe(
+        (like) => this.showAlert('curtiu '),
+        (err) => this.showAlert(err)
+      ); 
+  }
+
+  showAlert(message) {
+    let alert = this.alertCtrl.create({
+      message: message,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
 
 }
