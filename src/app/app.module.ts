81f2cb42781e181e446ce38bb536c3c30f50d4d4
Tyago Goulart <tyagogoulartn@gmail.com>
2017-06-02 00:02:26 -0300

    checa storage antes de abrir login e seta roots

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicApp, IonicErrorHandler, IonicModule, NavController } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { Geolocation } from '@ionic-native/geolocation';
@@ -10,11 +10,14 @@ import { HomePage } from '../pages/home/home';
 import { InstagramCardComponent } from './../components/instagram-card/instagram-card.component';
 import { InstagramService } from './../providers/instagram.service';
 import { IonicStorageModule } from '@ionic/storage';
+import { MediaPage } from './../pages/media/media';
 import { SettingsPage } from './../pages/settings/settings';
 import { SettingsMenuPage } from './../pages/settings-menu/settings-menu';
+import { LocationPage } from './../pages/location/location';
 import { LocationsPage } from './../pages/locations/locations';
 import { LoginPage } from './../pages/login/login';
 import { HttpModule } from "@angular/http";
+import { UserProfilePage } from './../pages/user-profile/user-profile';
 
 @NgModule({
   declarations: [
@@ -24,7 +27,10 @@ import { HttpModule } from "@angular/http";
     SettingsMenuPage,
     SettingsPage,
     LoginPage,
-    LocationsPage
+    LocationsPage,
+    UserProfilePage,
+    MediaPage,
+    LocationPage
   ],
   imports: [
     BrowserModule,
@@ -39,13 +45,17 @@ import { HttpModule } from "@angular/http";
     SettingsMenuPage,
     SettingsPage,
     LoginPage,
-    LocationsPage
+    LocationsPage,
+    UserProfilePage,
+    MediaPage,
+    LocationPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
     InstagramService,
     Geolocation,
+    Storage,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
